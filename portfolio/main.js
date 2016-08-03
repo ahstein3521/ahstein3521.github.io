d6b585be2d350181b7083760aa162ec116ae23fe
ahstein3521 <adamhs3521@gmail.com>
2016-08-03 19:30:01 -0400

    fixed dropdown

--- portfolio/main.js
+++ portfolio/main.js
@@ -2,9 +2,22 @@
 //All events related to Landing page dropdown and page scrolling
 function autoScroll(label){
   let height=$("."+label.toLowerCase()).offsetTop-$('nav').clientHeight;
-  
+  smoothScroll(height);
+}
+function smoothScroll(height){
   $('html body').animate({scrollTop:height},1000).css('overflow-y','scroll')
-  //http://stackoverflow.com/questions/832860/how-to-scroll-the-window-using-jquery-scrollto-function
+//http://stackoverflow.com/questions/832860/how-to-scroll-the-window-using-jquery-scrollto-function
+}
+
+function toggleScrollHeight(){
+  $("body").toggleClass("hide-scroll");
+
+  if($("body").hasClass("hide-scroll")){
+    $("html body").css("position","fixed").css("overflow-y","scroll")
+  }else{
+    smoothScroll(localStorage.getItem("top"))
+    $("html body").css('position','static');
+  }
 }
 
 window.addEventListener('scroll',function(){
@@ -14,7 +27,10 @@ window.addEventListener('scroll',function(){
 
 $(".nav-menu").click(function(){
   let _class=["top","middle","bottom"];
-  let scroll="hidden";
+  
+  if(document.body.scrollTop!=0){
+    localStorage.setItem("top",document.body.scrollTop)
+  }
   
   for(let i=0;i<3;i++){
     $(".nav-menu div:nth-child("+(i+1)+")").toggleClass(_class[i]);
@@ -24,9 +40,5 @@ $(".nav-menu").click(function(){
                    .children()
                    .toggleClass("fadeIn")
                    .toggleClass("fadeOut")
- 
-  $("body").toggleClass("hide-scroll");
-
-  if($("body").hasClass())
-
+  toggleScrollHeight();
 }) 
