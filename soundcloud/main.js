b311d71440538d13bc0899cde827ae901e050585
ahstein3521 <adamhs3521@gmail.com>
2016-08-07 20:52:08 -0400

    added toggling

--- soundcloud/main.js
+++ soundcloud/main.js
@@ -1,6 +1,13 @@
 const id='ddca176ba143988989db8adba1cdfa7f';
 const default_img="https://i.ytimg.com/vi/ekYcOzXXX0Q/hqdefault.jpg";
 
+const default_track={
+					  index:0,
+					    url:"https%3A//api.soundcloud.com/tracks/104570219",
+						art:"https://i1.sndcdn.com/artworks-000054832512-8zv6d9-large.jpg",
+					  title:"Frank Zappa- Willie The Pimp"
+					}
+
 $(document).ready(function(){
 
 	SC.initialize({client_id:id});
@@ -9,21 +16,27 @@ $(document).ready(function(){
 	var playlist={};//global object for the current playlist
 	var library=[];
 
-	(function(){
-		if(typeof localStorage['library']!="undefined"){
-			library=JSON.parse(localStorage.getItem("library"));
-			
-			var list=library.map(function(v){ return "<li class='playlist-link'>"+v.title+"</li>"}).join("");
-			$(".dropdown-menu").html(list)
-		}
-	})()
-
-	var PlayList=function(title){
-		this.title=title;
-		this.list=[];
-		this.currentTrack;
+	function uniqueTitle(title){
+		var titleArray=library.map((v)=>{return v.title.toLowerCase()});
+		 return titleArray.indexOf(title.toLowerCase());
+		
+		// titleArray=titleArray.filter((v)=> {return v==title});
+		// return [ index , title + "("+titleArray.length+")"];
 	}
 
+	var PlayList=function(title="Untitled"){
+		this.title=title;
+		this.list=[default_track];
+		this.currentTrack=default_track;
+	}
+
+	PlayList.prototype.setTitle=function(newTitle){
+		this.title=newTitle;
+	}
+	PlayList.prototype.init=function(){
+		this.currentTrack=this.list[0];
+		this.load(true);	
+	}
 	PlayList.prototype.next=function(){
 		var nextIndex=this.currentTrack.index+1;
 		if(nextIndex>=this.list.length) nextIndex=0;
@@ -57,12 +70,11 @@ $(document).ready(function(){
 		this.currentTrack=this.list[i];
 	}
 	
-	PlayList.prototype.load=function(){
+	PlayList.prototype.load=function(autoPlay){
 		let track=this.currentTrack;
-		
-		widget.load(track.url);
-		$(".screen").html("<img src='"+track.art+"' class='img'/><p>"+track.title+"</p><br><p> Track: "+ (+track.index+1)+" of "+this.list.length+" on <em>"+this.title+"</em>");
-		
+		if(autoPlay) $(".play span").removeClass("glyphicon-play").addClass("glyphicon-pause");
+		widget.load(track.url,{auto_play:autoPlay});
+		$(".screen").html("<img src='"+track.art+"' class='img'/><p class='title'><strong>"+track.title+"</strong></p><br><p class='track-listing'> Track: "+ (+track.index+1)+" of "+this.list.length+" on <em>"+this.title+"</em>");
 	}
 	
 	function buildList(arr){
@@ -70,21 +82,32 @@ $(document).ready(function(){
 			list+=arr.map((v,i)=>{
 			return "<li class='list-group-item'><button class='link' id='"+i+"'>"+v.title+"</button></li>"}).join("");
 			list+="</ul>"
-			$(".results").html(list);
+			$(".modal-body").html(list);
 	}
 
-	function togglePlayButton(){
-		$(".play").children("span").toggleClass("glyphicon-play").toggleClass("glyphicon-pause");
-	}
+	function togglePlayButton(){$(".play span").toggleClass("glyphicon-play").toggleClass("glyphicon-pause");}
 
-	$(".search-btn").click(function(e){
+	(function(){
+		if(typeof localStorage['library']!="undefined"){
+			library=JSON.parse(localStorage.getItem("library"));
+			
+			var list=library.map(function(v){ return "<li class='playlist-link'>"+v.title+"</li>"}).join("");
+			$(".dropdown-menu").html(list)
+		}	
+		playlist=new PlayList();
+		playlist.load(false);
+	})()
+
+	
+	$(".search-btn").on("click",function(e){
 		var query=$("input").val();
-		var titles=library.map(v=> v.title);
-        var index=titles.indexOf(query)
 
-		playlist=new PlayList(query)
+		if(query.trim().length==0) return;
 		
-		if(index!=-1){
+		var index=uniqueTitle(query);
+		playlist=new PlayList(query);
+
+		if(index!==-1){
 			playlist.list=library[index].list;
 			return buildList(playlist.list);
 		}
@@ -93,36 +116,27 @@ $(document).ready(function(){
 	  	  .then(function(tracks) {
 	  	  	buildList(tracks);
 	  	  	playlist.build(tracks);
-	  	  	playlist.save();
 	  	});
 	})
-		SC.get('/tracks', {q: "lenny kravitz"})
-	  	  .then(function(tracks) {
-	  	  	playlist=new PlayList("lenny kravitz")
-	  	  	buildList(tracks);
-	  	  	playlist.build(tracks);
-	  	  	playlist.save();
-	  	});
-
 
 	$(document).on("click",".link",function(){
-		playlist.select(this)
-		playlist.load();
-
+		togglePlayButton();
+		playlist.select(this);
+		playlist.load(true);
+		
 	})
 		$(document).on("click",".back",function(){
+		if(playlist.list.length==1)return;		
 		playlist.back()
-		playlist.load();
-		widget.play()
+		playlist.load(true);
+	
 	})
 		$(document).on("click",".next",function(){
+		if(playlist.list.length==1)return;	
 		playlist.next();
-		playlist.load();
-		widget.play();
+		playlist.load(true);
+		
 	})
-
-
-	
 
 	$(".play").on("click",function(){
 		togglePlayButton();
@@ -131,5 +145,16 @@ $(document).ready(function(){
 		}else{
 			widget.pause();
 		}
-	})	
+	})
+	
+	$(".save-playlist").on("click",function(){
+		playlist.save();
+		$(".playlist").modal("hide");
+	})
+	
+	$(".load-playlist").on("click",function(){
+		playlist.init();
+		$(".playlist").modal("hide");
+	})
+		
 })
